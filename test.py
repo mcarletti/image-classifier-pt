e2320ebb997567a74a72ff218d49e58ca67ec540
mcarletti <marco.carletti@univr.it>
2017-11-16 13:46:13 +0100

    Add test single image

--- test.py
+++ test.py
@@ -8,6 +8,15 @@ import network
 import matplotlib.pyplot as plt
 import numpy as np
 
+from PIL import Image
+import os
+
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_image', type=str, default=None)
+args = parser.parse_args()
 
 HAS_CUDA = True
 if not torch.cuda.is_available():
@@ -18,35 +27,59 @@ mu = (0.485, 0.456, 0.406)
 sd = (0.229, 0.224, 0.225)
 transf = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mu, sd)])
 
-print('Loading dataset')
-test_set = dataset.DATASET(train=False, transform=transf)
-class_names = test_set.labels
-nb_classes = class_names.shape[0]
-
-batch_size = 16
-test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=True, num_workers=0)
-
-
 print('Creating network')
 net = torch.load('trained_model.pth')
+net.eval()
 if HAS_CUDA:
     net.cuda()
 
+if args.input_image is not None:
 
-print('Computing accuracy on testing set')
-net.eval()
-correct = 0
-total = 0
-for data in test_loader:
-    images, targets, _ = data
+    assert os.path.exists(args.input_image)
 
+    print('Load image')
+    image = np.asarray(Image.open(args.input_image).resize((100, 100)))
+    assert(image.shape == (100, 100, 3))
+
+    plt.imshow(image)
+    plt.show()
+
+    image = transf(image).unsqueeze(0)
+    image = torch.autograd.Variable(image)
     if HAS_CUDA:
-        images, targets = images.cuda(), targets.type(torch.LongTensor).cuda()
+        image = image.cuda()
 
-    predictions = net(torch.autograd.Variable(images, requires_grad=False))
-    _, predicted = torch.max(predictions.data, 1)
-    total += batch_size
-    correct += (predicted == targets).sum()
+    print('Computing prediction')
+    pred = net(image)
+    pred = pred.data.cpu().numpy()
 
-print('Accuracy of the network on the testing images: %d %%' % (100 * correct / total))
+    class_id = np.argmax(pred)
+    class_labels = np.loadtxt(open('data/bin_data/class_labels.txt'), dtype=object, delimiter='\n')
+    print(class_labels[class_id])
+
+else:
+
+    print('Loading dataset')
+    test_set = dataset.DATASET(train=False, transform=transf)
+    class_names = test_set.labels
+    nb_classes = class_names.shape[0]
+
+    batch_size = 16
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=True, num_workers=0)
+
+    print('Computing accuracy on testing set')
+    correct = 0
+    total = 0
+    for data in test_loader:
+        images, targets, _ = data
+
+        if HAS_CUDA:
+            images, targets = images.cuda(), targets.type(torch.LongTensor).cuda()
+
+        predictions = net(torch.autograd.Variable(images, requires_grad=False))
+        _, predicted = torch.max(predictions.data, 1)
+        total += batch_size
+        correct += (predicted == targets).sum()
+
+    print('Accuracy of the network on the testing images: %d %%' % (100 * correct / total))
 
