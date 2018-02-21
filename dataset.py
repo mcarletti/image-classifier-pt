fcd3853ebfdcd6d77e405faa907861c237b7acc6
mcarletti <marco.carletti@univr.it>
2018-02-21 13:38:14 +0100

    Add parent argument to dataset

--- dataset.py
+++ dataset.py
@@ -1,12 +1,8 @@
-from __future__ import print_function
 import torch
 import scipy.io as sio
 from PIL import Image
 import numpy as np
-
-
-#%% ---------------------------------------------------------------------------
-
+import os
 
 class DATASET(torch.utils.data.Dataset):
     '''
@@ -18,17 +14,17 @@ class DATASET(torch.utils.data.Dataset):
     
     '''
 
-    def __init__(self, train=True, transform=None):
+    def __init__(self, parent, train=True, transform=None):
         if train:
-            self.matfile = './data/bin_data/train.mat'
+            self.matfile = os.path.join(parent, 'train.mat')
         else:
-            self.matfile = './data/bin_data/test.mat'
+            self.matfile = os.path.join(parent, 'test.mat')
         self.transform = transform
 
         data = sio.loadmat(self.matfile)
         self.images = np.squeeze(data['images'])
         self.values = np.squeeze(data['values'])
-        self.labels = np.squeeze(data['labels'])
+        #self.labels = np.squeeze(data['labels'])
 
     def __getitem__(self, index):
         '''
@@ -45,7 +41,7 @@ class DATASET(torch.utils.data.Dataset):
         if self.transform is not None:
             image = self.transform(image)
 
-        return image, target, str(self.labels[target])
+        return image, target #, str(self.labels[target])
 
     def __len__(self):
         return len(self.images)
